d8edd2a441ee584db30180b5be2e403da5576217
drewster1123 <72901824+drewster1123@users.noreply.github.com>
2020-12-09 21:58:33 -0500

    Final project commented

--- Final Project/Final Project/Form1.Designer.vb
+++ Final Project/Final Project/Form1.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
-        Me.Cloud1 = New System.Windows.Forms.PictureBox()
         Me.Player = New System.Windows.Forms.PictureBox()
         Me.PictureBox6 = New System.Windows.Forms.PictureBox()
         Me.PictureBox5 = New System.Windows.Forms.PictureBox()
@@ -39,18 +38,6 @@ Partial Class Form1
         Me.Jumphb4 = New System.Windows.Forms.PictureBox()
         Me.Jumphb5 = New System.Windows.Forms.PictureBox()
         Me.Jumphb6 = New System.Windows.Forms.PictureBox()
-        Me.Floor1L = New System.Windows.Forms.PictureBox()
-        Me.Floor1R = New System.Windows.Forms.PictureBox()
-        Me.Floor2L = New System.Windows.Forms.PictureBox()
-        Me.Floor2R = New System.Windows.Forms.PictureBox()
-        Me.Floor6L = New System.Windows.Forms.PictureBox()
-        Me.Floor6R = New System.Windows.Forms.PictureBox()
-        Me.Floor5L = New System.Windows.Forms.PictureBox()
-        Me.Floor5R = New System.Windows.Forms.PictureBox()
-        Me.Floor4L = New System.Windows.Forms.PictureBox()
-        Me.Floor4R = New System.Windows.Forms.PictureBox()
-        Me.Floor3R = New System.Windows.Forms.PictureBox()
-        Me.Floor3L = New System.Windows.Forms.PictureBox()
         Me.Bottom1 = New System.Windows.Forms.PictureBox()
         Me.Bottom2 = New System.Windows.Forms.PictureBox()
         Me.Bottom3 = New System.Windows.Forms.PictureBox()
@@ -60,7 +47,7 @@ Partial Class Form1
         Me.dead = New System.Windows.Forms.PictureBox()
         Me.GameOverBack = New System.Windows.Forms.PictureBox()
         Me.gameOver = New System.Windows.Forms.Label()
-        CType(Me.Cloud1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Cloud1 = New System.Windows.Forms.PictureBox()
         CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -74,18 +61,6 @@ Partial Class Form1
         CType(Me.Jumphb4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Jumphb5, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Jumphb6, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor1L, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor1R, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor2L, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor2R, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor6L, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor6R, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor5L, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor5R, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor4L, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor4R, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor3R, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Floor3L, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Bottom1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Bottom2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Bottom3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -94,6 +69,7 @@ Partial Class Form1
         CType(Me.Bottom6, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.dead, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GameOverBack, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Cloud1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Timer1
@@ -105,16 +81,6 @@ Partial Class Form1
         '
         Me.Timer2.Enabled = True
         Me.Timer2.Interval = 25
-        '
-        'Cloud1
-        '
-        Me.Cloud1.Image = Global.Final_Project.My.Resources.Resources.Cloud_PNG_4
-        Me.Cloud1.Location = New System.Drawing.Point(68, 33)
-        Me.Cloud1.Name = "Cloud1"
-        Me.Cloud1.Size = New System.Drawing.Size(203, 111)
-        Me.Cloud1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.Cloud1.TabIndex = 7
-        Me.Cloud1.TabStop = False
         '
         'Player
         '
@@ -130,7 +96,7 @@ Partial Class Form1
         Me.PictureBox6.Image = Global.Final_Project.My.Resources.Resources.Project_Floor_just_bricks
         Me.PictureBox6.Location = New System.Drawing.Point(670, 222)
         Me.PictureBox6.Name = "PictureBox6"
-        Me.PictureBox6.Size = New System.Drawing.Size(202, 60)
+        Me.PictureBox6.Size = New System.Drawing.Size(202, 34)
         Me.PictureBox6.TabIndex = 5
         Me.PictureBox6.TabStop = False
         '
@@ -139,25 +105,25 @@ Partial Class Form1
         Me.PictureBox5.Image = Global.Final_Project.My.Resources.Resources.Project_Floor_just_bricks
         Me.PictureBox5.Location = New System.Drawing.Point(1137, 314)
         Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(281, 60)
+        Me.PictureBox5.Size = New System.Drawing.Size(281, 34)
         Me.PictureBox5.TabIndex = 4
         Me.PictureBox5.TabStop = False
         '
         'PictureBox4
         '
         Me.PictureBox4.Image = Global.Final_Project.My.Resources.Resources.Project_Floor_just_bricks
-        Me.PictureBox4.Location = New System.Drawing.Point(1590, 454)
+        Me.PictureBox4.Location = New System.Drawing.Point(1638, 397)
         Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(286, 60)
+        Me.PictureBox4.Size = New System.Drawing.Size(286, 34)
         Me.PictureBox4.TabIndex = 3
         Me.PictureBox4.TabStop = False
         '
         'PictureBox3
         '
         Me.PictureBox3.Image = Global.Final_Project.My.Resources.Resources.Project_Floor_just_bricks
-        Me.PictureBox3.Location = New System.Drawing.Point(1246, 589)
+        Me.PictureBox3.Location = New System.Drawing.Point(1309, 569)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(281, 60)
+        Me.PictureBox3.Size = New System.Drawing.Size(281, 35)
         Me.PictureBox3.TabIndex = 2
         Me.PictureBox3.TabStop = False
         '
@@ -166,7 +132,7 @@ Partial Class Form1
         Me.PictureBox2.Image = Global.Final_Project.My.Resources.Resources.Project_Floor_just_bricks
         Me.PictureBox2.Location = New System.Drawing.Point(821, 693)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(337, 60)
+        Me.PictureBox2.Size = New System.Drawing.Size(337, 35)
         Me.PictureBox2.TabIndex = 1
         Me.PictureBox2.TabStop = False
         '
@@ -175,7 +141,7 @@ Partial Class Form1
         Me.PictureBox1.Image = Global.Final_Project.My.Resources.Resources.Project_Floor_just_bricks
         Me.PictureBox1.Location = New System.Drawing.Point(28, 695)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(559, 60)
+        Me.PictureBox1.Size = New System.Drawing.Size(559, 33)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
@@ -200,7 +166,7 @@ Partial Class Form1
         'Jumphb3
         '
         Me.Jumphb3.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.Jumphb3.Location = New System.Drawing.Point(1263, 569)
+        Me.Jumphb3.Location = New System.Drawing.Point(1309, 553)
         Me.Jumphb3.Name = "Jumphb3"
         Me.Jumphb3.Size = New System.Drawing.Size(259, 10)
         Me.Jumphb3.TabIndex = 10
@@ -209,7 +175,7 @@ Partial Class Form1
         'Jumphb4
         '
         Me.Jumphb4.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.Jumphb4.Location = New System.Drawing.Point(1640, 401)
+        Me.Jumphb4.Location = New System.Drawing.Point(1665, 381)
         Me.Jumphb4.Name = "Jumphb4"
         Me.Jumphb4.Size = New System.Drawing.Size(259, 10)
         Me.Jumphb4.TabIndex = 11
@@ -233,114 +199,6 @@ Partial Class Form1
         Me.Jumphb6.TabIndex = 13
         Me.Jumphb6.TabStop = False
         '
-        'Floor1L
-        '
-        Me.Floor1L.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor1L.Location = New System.Drawing.Point(12, 695)
-        Me.Floor1L.Name = "Floor1L"
-        Me.Floor1L.Size = New System.Drawing.Size(10, 60)
-        Me.Floor1L.TabIndex = 14
-        Me.Floor1L.TabStop = False
-        '
-        'Floor1R
-        '
-        Me.Floor1R.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor1R.Location = New System.Drawing.Point(593, 693)
-        Me.Floor1R.Name = "Floor1R"
-        Me.Floor1R.Size = New System.Drawing.Size(10, 60)
-        Me.Floor1R.TabIndex = 15
-        Me.Floor1R.TabStop = False
-        '
-        'Floor2L
-        '
-        Me.Floor2L.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor2L.Location = New System.Drawing.Point(821, 627)
-        Me.Floor2L.Name = "Floor2L"
-        Me.Floor2L.Size = New System.Drawing.Size(10, 60)
-        Me.Floor2L.TabIndex = 16
-        Me.Floor2L.TabStop = False
-        '
-        'Floor2R
-        '
-        Me.Floor2R.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor2R.Location = New System.Drawing.Point(1148, 627)
-        Me.Floor2R.Name = "Floor2R"
-        Me.Floor2R.Size = New System.Drawing.Size(10, 60)
-        Me.Floor2R.TabIndex = 17
-        Me.Floor2R.TabStop = False
-        '
-        'Floor6L
-        '
-        Me.Floor6L.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor6L.Location = New System.Drawing.Point(634, 220)
-        Me.Floor6L.Name = "Floor6L"
-        Me.Floor6L.Size = New System.Drawing.Size(10, 60)
-        Me.Floor6L.TabIndex = 18
-        Me.Floor6L.TabStop = False
-        '
-        'Floor6R
-        '
-        Me.Floor6R.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor6R.Location = New System.Drawing.Point(924, 220)
-        Me.Floor6R.Name = "Floor6R"
-        Me.Floor6R.Size = New System.Drawing.Size(10, 60)
-        Me.Floor6R.TabIndex = 19
-        Me.Floor6R.TabStop = False
-        '
-        'Floor5L
-        '
-        Me.Floor5L.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor5L.Location = New System.Drawing.Point(1126, 303)
-        Me.Floor5L.Name = "Floor5L"
-        Me.Floor5L.Size = New System.Drawing.Size(10, 60)
-        Me.Floor5L.TabIndex = 20
-        Me.Floor5L.TabStop = False
-        '
-        'Floor5R
-        '
-        Me.Floor5R.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor5R.Location = New System.Drawing.Point(1439, 312)
-        Me.Floor5R.Name = "Floor5R"
-        Me.Floor5R.Size = New System.Drawing.Size(10, 60)
-        Me.Floor5R.TabIndex = 21
-        Me.Floor5R.TabStop = False
-        '
-        'Floor4L
-        '
-        Me.Floor4L.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor4L.Location = New System.Drawing.Point(1613, 386)
-        Me.Floor4L.Name = "Floor4L"
-        Me.Floor4L.Size = New System.Drawing.Size(10, 60)
-        Me.Floor4L.TabIndex = 22
-        Me.Floor4L.TabStop = False
-        '
-        'Floor4R
-        '
-        Me.Floor4R.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor4R.Location = New System.Drawing.Point(1872, 493)
-        Me.Floor4R.Name = "Floor4R"
-        Me.Floor4R.Size = New System.Drawing.Size(10, 60)
-        Me.Floor4R.TabIndex = 23
-        Me.Floor4R.TabStop = False
-        '
-        'Floor3R
-        '
-        Me.Floor3R.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor3R.Location = New System.Drawing.Point(1538, 587)
-        Me.Floor3R.Name = "Floor3R"
-        Me.Floor3R.Size = New System.Drawing.Size(10, 60)
-        Me.Floor3R.TabIndex = 24
-        Me.Floor3R.TabStop = False
-        '
-        'Floor3L
-        '
-        Me.Floor3L.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Floor3L.Location = New System.Drawing.Point(1218, 569)
-        Me.Floor3L.Name = "Floor3L"
-        Me.Floor3L.Size = New System.Drawing.Size(10, 60)
-        Me.Floor3L.TabIndex = 25
-        Me.Floor3L.TabStop = False
-        '
         'Bottom1
         '
         Me.Bottom1.BackColor = System.Drawing.SystemColors.ButtonShadow
@@ -362,7 +220,7 @@ Partial Class Form1
         'Bottom3
         '
         Me.Bottom3.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.Bottom3.Location = New System.Drawing.Point(1263, 656)
+        Me.Bottom3.Location = New System.Drawing.Point(1318, 610)
         Me.Bottom3.Name = "Bottom3"
         Me.Bottom3.Size = New System.Drawing.Size(259, 10)
         Me.Bottom3.TabIndex = 28
@@ -371,7 +229,7 @@ Partial Class Form1
         'Bottom4
         '
         Me.Bottom4.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.Bottom4.Location = New System.Drawing.Point(1622, 539)
+        Me.Bottom4.Location = New System.Drawing.Point(1676, 449)
         Me.Bottom4.Name = "Bottom4"
         Me.Bottom4.Size = New System.Drawing.Size(259, 10)
         Me.Bottom4.TabIndex = 29
@@ -425,6 +283,16 @@ Partial Class Form1
         Me.gameOver.TabIndex = 34
         Me.gameOver.Text = "Game Over"
         '
+        'Cloud1
+        '
+        Me.Cloud1.Image = Global.Final_Project.My.Resources.Resources.Cloud_PNG_4
+        Me.Cloud1.Location = New System.Drawing.Point(68, 33)
+        Me.Cloud1.Name = "Cloud1"
+        Me.Cloud1.Size = New System.Drawing.Size(203, 111)
+        Me.Cloud1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Cloud1.TabIndex = 7
+        Me.Cloud1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -440,18 +308,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Bottom3)
         Me.Controls.Add(Me.Bottom2)
         Me.Controls.Add(Me.Bottom1)
-        Me.Controls.Add(Me.Floor3L)
-        Me.Controls.Add(Me.Floor3R)
-        Me.Controls.Add(Me.Floor4R)
-        Me.Controls.Add(Me.Floor4L)
-        Me.Controls.Add(Me.Floor5R)
-        Me.Controls.Add(Me.Floor5L)
-        Me.Controls.Add(Me.Floor6R)
-        Me.Controls.Add(Me.Floor6L)
-        Me.Controls.Add(Me.Floor2R)
-        Me.Controls.Add(Me.Floor2L)
-        Me.Controls.Add(Me.Floor1R)
-        Me.Controls.Add(Me.Floor1L)
         Me.Controls.Add(Me.Jumphb6)
         Me.Controls.Add(Me.Jumphb5)
         Me.Controls.Add(Me.Jumphb4)
@@ -468,7 +324,6 @@ Partial Class Form1
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.Cloud1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Player, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
@@ -482,18 +337,6 @@ Partial Class Form1
         CType(Me.Jumphb4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Jumphb5, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Jumphb6, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor1L, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor1R, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor2L, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor2R, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor6L, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor6R, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor5L, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor5R, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor4L, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor4R, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor3R, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Floor3L, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Bottom1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Bottom2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Bottom3, System.ComponentModel.ISupportInitialize).EndInit()
@@ -502,6 +345,7 @@ Partial Class Form1
         CType(Me.Bottom6, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.dead, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GameOverBack, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Cloud1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -516,25 +360,12 @@ Partial Class Form1
     Friend WithEvents PictureBox6 As PictureBox
     Friend WithEvents Player As PictureBox
     Friend WithEvents Timer2 As Timer
-    Friend WithEvents Cloud1 As PictureBox
     Friend WithEvents Jumphb1 As PictureBox
     Friend WithEvents Jumphb2 As PictureBox
     Friend WithEvents Jumphb3 As PictureBox
     Friend WithEvents Jumphb4 As PictureBox
     Friend WithEvents Jumphb5 As PictureBox
     Friend WithEvents Jumphb6 As PictureBox
-    Friend WithEvents Floor1L As PictureBox
-    Friend WithEvents Floor1R As PictureBox
-    Friend WithEvents Floor2L As PictureBox
-    Friend WithEvents Floor2R As PictureBox
-    Friend WithEvents Floor6L As PictureBox
-    Friend WithEvents Floor6R As PictureBox
-    Friend WithEvents Floor5L As PictureBox
-    Friend WithEvents Floor5R As PictureBox
-    Friend WithEvents Floor4L As PictureBox
-    Friend WithEvents Floor4R As PictureBox
-    Friend WithEvents Floor3R As PictureBox
-    Friend WithEvents Floor3L As PictureBox
     Friend WithEvents Bottom1 As PictureBox
     Friend WithEvents Bottom2 As PictureBox
     Friend WithEvents Bottom3 As PictureBox
@@ -544,4 +375,5 @@ Partial Class Form1
     Friend WithEvents dead As PictureBox
     Friend WithEvents GameOverBack As PictureBox
     Friend WithEvents gameOver As Label
+    Friend WithEvents Cloud1 As PictureBox
 End Class
